ef0e2dc1ed4b023e4ac91b75c362453f0f41f3df
taylorbrian77 <taylorbrian77@gmail.com>
2018-10-06 17:56:23 -0400

    ready for you alex!

---
+++ src/control/control.py
@@ -0,0 +1,19 @@
+'''
+purpose of class here (get rid of todo once done)
+
+TODO: add explanation in readme file, build skeleton
+'''
+class Control:
+    def __init__(x):
+        '''
+        <description>: delete this constructor when you're done with it alex,
+        keep variables lower case and under score dO nOT uSeD cAmeL cAsE
+
+        Args:
+            x(int): you're explanation here
+
+        Returns:
+            int: what gets returned here
+        '''
+        pass
+    
