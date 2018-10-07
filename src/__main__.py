998106dab514a35d26380d93c0d9647a1489ce06
randypowell10 <randypowell10@yahoo.com>
2018-10-07 00:32:59 -0400

    migrate flask app to __main__

--- src/__main__.py
+++ src/__main__.py
@@ -5,33 +5,47 @@ from modes import Driver
 2. modify to use picamera
 3. import Driver and pass to frame
 '''
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request, Response
 import cv2
-from picamera import PiCamera
+from opencvutils import Camera
+# from picamera import PiCamera
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return render_template('index.html')
+    if request.method == 'POST':
+        key = request.form['key']
+        action = request.form['action']
+        print("KEY: ", key," ACTION:", action)
+    return render_template('index.html')
+
+# def process_frame():
+#     driver = Driver()
+
+#     self.camera = PiCamera()
+#     self.camera.resolution = (640, 480)
+#     self.camera.framerate = 32
+#     rawCapture = PiRGBArray(camera, size=(640, 480))
+#     time.sleep(0.1)
+#     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#         # show the frame
+#         cv2.imshow("Frame", image)
+#         # clear the stream in preparation for the next frame
+#         rawCapture.truncate(0)
+
+#         frame = driver.frame(frame)
+	
+#         ret, jpg = cv2.imencode('.jpg', frame)
+#         yield (b'--jpgboundary\r\n'+ 
+#             b'Content-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n')
 
 def process_frame():
-    driver = Driver()
-
-    self.camera = PiCamera()
-    self.camera.resolution = (640, 480)
-    self.camera.framerate = 32
-    rawCapture = PiRGBArray(camera, size=(640, 480))
-    time.sleep(0.1)
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # show the frame
-        cv2.imshow("Frame", image)
-        # clear the stream in preparation for the next frame
-        rawCapture.truncate(0)
-
-        frame = driver.frame(frame)
-	
+    cap = cv2.VideoCapture(0)
+    print ("capture:", cap)
+    while True:
+        ret, frame = cap.read()
         ret, jpg = cv2.imencode('.jpg', frame)
-        yield (b'--jpgboundary\r\n'+ 
+        yield (b'--jpgboundary\r\n'+
             b'Content-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n')
 
 @app.route('/video_feed')
