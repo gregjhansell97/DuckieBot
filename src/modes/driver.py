e7b287588f8bc6513048ff7eebc74be33e762b88
gregoryjhansell97 <gregjhansell@gmail.com>
2018-10-06 21:47:35 -0400

    stubbed driver

--- src/modes/driver.py
+++ src/modes/driver.py
@@ -1,7 +1,4 @@
-#from Controls import ControlDebug
-from picamera import PiCamera
-import cv2
-
+#from Controls import Control
 
 class Driver:
     '''
@@ -9,18 +6,29 @@ class Driver:
     def __init__(self):
         self._keys_pressed = []
         self._keys_released = []
-
-        self.camera = PiCamera()
-        self.camera.resolution(REPLACE, REPLACE)
-        self.camera.framerate = FRAME_RATE
-
+        self.speed_accel = 0.2
+        self.omega_accel = 0.2
+        self.speed = 0
+        self.omega = 0
 
     def set_input(next_pressed=[], next_released=[]):
-        pass
+        print("#"*50)
+        print(next_pressed)
+        print(next_released)
+        print("#"*50)
+        return
+
+        if "up" in next_released or "down" in next_released:
+            self.speed = 0
+        elif "right" in next_released or "left" in next_released:
+            self.omega = 0
+        
+        if "up" in next_pressed:
+            self.speed += 
+      
+        self._keys_pressed = 
+       pass
 
     def frame(self, frame):
         return frame
-
-    def _modify_frame(self, frame):
-        pass        
         
