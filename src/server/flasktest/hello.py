2a2d566d8a7cb637fd532f47e5e58410c768a2eb
randypowell10 <randypowell10@yahoo.com>
2018-10-06 23:26:05 -0400

    Flask app with operational requests

--- src/server/flasktest/hello.py
+++ src/server/flasktest/hello.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import cv2
 from opencvutils import Camera
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+	if request.method == 'POST':
+		key = request.form['key']
+		action = request.form['action']
+		print("KEY: ", key," ACTION:", action)
 	return render_template('index.html')
 
 
@@ -17,8 +21,7 @@ def gen():
 		ret, jpg = cv2.imencode('.jpg', frame)
 		yield (b'--jpgboundary\r\n'+
 			b'Content-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n')
-def frame_callback(frame):
-    
+
 
 @app.route('/video_feed')
 def video_feed():
