b71b9465af413e53ee474e7e487aa3bb1de5d885
randypowell10 <randypowell10@yahoo.com>
2018-10-06 23:31:46 -0400

    tabs to spaces

--- src/server/flasktest/hello.py
+++ src/server/flasktest/hello.py
@@ -6,27 +6,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-	if request.method == 'POST':
-		key = request.form['key']
-		action = request.form['action']
-		print("KEY: ", key," ACTION:", action)
-	return render_template('index.html')
+    if request.method == 'POST':
+        key = request.form['key']
+        action = request.form['action']
+        print("KEY: ", key," ACTION:", action)
+    return render_template('index.html')
 
 
 def gen():
-	cap = cv2.VideoCapture(0)
-	print ("capture:", cap)
-	while True:
-		ret, frame = cap.read()
-		ret, jpg = cv2.imencode('.jpg', frame)
-		yield (b'--jpgboundary\r\n'+
-			b'Content-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n')
+    cap = cv2.VideoCapture(0)
+    print ("capture:", cap)
+    while True:
+        ret, frame = cap.read()
+        ret, jpg = cv2.imencode('.jpg', frame)
+        yield (b'--jpgboundary\r\n'+
+            b'Content-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
-	return Response(gen(),
-			mimetype='multipart/x-mixed-replace; boundary=--jpgboundary')
+    return Response(gen(),
+            mimetype='multipart/x-mixed-replace; boundary=--jpgboundary')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
