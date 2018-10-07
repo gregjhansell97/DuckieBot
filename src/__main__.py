50df7afc891ceb11039872eff3746c31134c4e70
gregjhansell97 <gregjhansell@gmail.com>
2018-10-07 02:41:11 -0400

    camera fully functioning on RPi

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 from modes import Driver
-'''
-1. bring randy's flask here and run it from main
-2. modify to use picamera
-3. import Driver and pass to frame
-'''
 from flask import Flask, render_template, request, Response
 import cv2
 from opencvutils import Camera
-# from picamera import PiCamera
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import time
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
@@ -24,32 +21,35 @@ def index():
         print("KEY: ", key," ACTION:", action)
     return render_template('index.html')
 
-# def process_frame():
-#     driver = Driver()
-
-#     self.camera = PiCamera()
-#     self.camera.resolution = (640, 480)
-#     self.camera.framerate = 32
-#     rawCapture = PiRGBArray(camera, size=(640, 480))
-#     time.sleep(0.1)
-#     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-#         # show the frame
-#         cv2.imshow("Frame", image)
-#         # clear the stream in preparation for the next frame
-#         rawCapture.truncate(0)
-
-#         frame = driver.frame(frame)
-	
-#         ret, jpg = cv2.imencode('.jpg', frame)
-#         yield (b'--jpgboundary\r\n'+ 
-#             b'Content-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n')
-
 def process_frame():
     '''
     Function to process camera frames continuously and send them to the app
     
     Returns: no return value, constantly yields jpg images to front end
     '''
+    driver = Driver()
+    #frame = driver.frame(frame)
+
+    # initialize the camera and grab a reference to the raw camera capture
+    camera = PiCamera(framerate=32, resolution=(640, 480))
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+
+    # allow the camera to warmup
+    time.sleep(0.1)
+
+    # capture frames from the camera
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        image = frame.array
+        # show the frame
+        ret, jpg = cv2.imencode('.jpg', image)
+        yield (b'--jpgboundary\r\n'+
+            b'Content-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n')
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+
+def process_frame_pc():
     cap = cv2.VideoCapture(0)
     print ("capture:", cap)
     while True:
@@ -70,4 +70,4 @@ def video_feed():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+     app.run(host='0.0.0.0')
